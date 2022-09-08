67972ffcc1de0f4fed51a0f66960d26861afd50e
Stavros Kounis <skounis@gmail.com>
2022-09-08 16:40:10 +0200

    feat(demo) Setup UI and demonstration.

--- src/demo/App.js
+++ src/demo/App.js
@@ -2,14 +2,58 @@ import './css/App.css';
 import Lib from './../lib';
 
 class App {
+  inputs = [];
   constructor() {
-    let libInstance = new Lib();
-    let input = libInstance.getReference();
-    // console.log(input);
-    // console.log("Demo loaded!", libInstance);
+    document.getElementById("calculate").addEventListener("click", this.calculate);
+    this.calculate();
     this.demoArrowMethod();
   }
 
+  calculate = () => {
+    console.log("Calculating...");
+    const keys = {
+      reference: [
+        'inflationRate',
+        'startBenefitAge',
+        'startBenefitPerMonth',
+        'benefitSVAge',
+        'benefitSVPerMonth',
+        'returnRateSafe',
+        'returnRateModerate',
+        'returnRateBold',
+        'durationAge',
+      ],
+      input: [
+        'client',
+        'capital',
+        'age',
+        'annualIncome']
+    }
+
+    const reference = Lib.GetReference();
+    const input = Lib.GetInput();
+
+    keys.reference.forEach(key => {
+      this.inputs[key] = document.querySelector(`#${key}`).valueAsNumber;
+      reference[key] = this.inputs[key]
+    });
+
+    keys.input.forEach(key => {
+      this.inputs[key] = document.querySelector(`#${key}`).valueAsNumber;
+      input[key] = this.inputs[key]
+    });
+
+    const libInstance = new Lib(reference, input);
+    const report = libInstance.report;
+    console.log(report)
+    const b = report.bold()
+    Object.keys(report.KEYS).forEach((key) => {
+      document.getElementById(`${key}[safe]`).value = report.safe()[key];
+      document.getElementById(`${key}[moderate]`).value = report.moderate()[key];
+      document.getElementById(`${key}[bold]`).value = report.bold()[key];
+    });
+  }
+  
   demoArrowMethod = () => {
     // console.log("Arrow methods will work");
   }
